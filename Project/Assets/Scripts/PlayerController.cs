78fc37ba420c22b0fae608a22977b7329e471e68
Syafiqjos <syafiqjos@gmail.com>
2021-05-12 17:15:21 +0700

    Add player movement

--- Project/Assets/Scripts/PlayerController.cs
+++ Project/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,73 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Properties")]
+    [SerializeField] private SpriteRenderer graphic;
+
+    [Header("Configuration")]
+    [SerializeField] private float moveSpeed = 1.0f;
+    [SerializeField] private float jumpForce = 2.0f;
+
+    private Rigidbody2D rb2;
+    private bool isGrounded = true;
+
     void Start()
     {
-        
+        rb2 = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        MovementController();
+        JumpController();
+        ShootController();
+    }
+
+    void MovementController()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+
+        Vector2 direction = rb2.velocity;
+        direction.x = x * moveSpeed;
+
+        rb2.velocity = direction;
+
+        //sprite dibalik ketika arahnya ke kiri
+        if (direction.x < 0)
+        {
+            graphic.flipX = true;
+        } else if (direction.x > 0)
+        {
+            graphic.flipX = false;
+        }
+    }
+
+    void JumpController()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        if (isGrounded)
+        {
+            rb2.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    void ShootController()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+
     }
 }
